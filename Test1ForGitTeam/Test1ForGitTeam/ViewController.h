d86bffb5a8b656da75141fdb60bbbda436ddcb01
xu54 <xu54@me.com>
2015-12-27 14:22:34 +0800

    commit1

---
+++ Test1ForGitTeam/Test1ForGitTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1ForGitTeam
+//
+//  Created by xu54 on 15/12/27.
+//  Copyright © 2015年 xu54. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test1ForGitTeam/Test1ForGitTeamTests/Test1ForGitTeamTests.m
@@ -0,0 +1,39 @@
+//
+//  Test1ForGitTeamTests.m
+//  Test1ForGitTeamTests
+//
+//  Created by xu54 on 15/12/27.
+//  Copyright © 2015年 xu54. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test1ForGitTeamTests : XCTestCase
+
+@end
+
+@implementation Test1ForGitTeamTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
